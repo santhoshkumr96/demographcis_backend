5728e33a686c2a143dccb5397b4b2ae9ec2cde7d
santhoshkumr96 <santhoshkumr96@gmail.com>
2023-01-13 12:42:38 +0530

    refactor

--- src/main/java/com/aurum/demographics/utils/JwtUtils.java
+++ src/main/java/com/aurum/demographics/utils/JwtUtils.java
@@ -19,13 +19,13 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${demographics.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${demographics.app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
-  @Value("${bezkoder.app.jwtCookieName}")
+  @Value("${demographics.app.jwtCookieName}")
   private String jwtCookie;
 
   public String getJwtFromCookies(HttpServletRequest request) {
