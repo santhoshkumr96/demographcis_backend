f0fa25e69ff20cd57076650c90f7cd2d95d17b22
santhoshkumr96 <santhoshkumr96@gmail.com>
2023-01-13 12:15:19 +0530

    remove encoding validation

--- src/main/java/com/aurum/demographics/contorller/AuthController.java
+++ src/main/java/com/aurum/demographics/contorller/AuthController.java
@@ -84,7 +84,7 @@ public class AuthController {
     // Create new user's account
     User user = new User(signUpRequest.getUsername(),
                          signUpRequest.getEmail(),
-                         encoder.encode(signUpRequest.getPassword()));
+                         signUpRequest.getPassword());
 
     Set<String> strRoles = signUpRequest.getRole();
     Set<Role> roles = new HashSet<>();
