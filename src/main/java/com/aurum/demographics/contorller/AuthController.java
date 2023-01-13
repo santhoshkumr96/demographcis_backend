87328ca60a88fabb9c70193e5d5610aeeac92073
santhoshkumr96 <santhoshkumr96@gmail.com>
2023-01-13 12:14:58 +0530

    remove email validation

--- src/main/java/com/aurum/demographics/contorller/AuthController.java
+++ src/main/java/com/aurum/demographics/contorller/AuthController.java
@@ -10,8 +10,10 @@ import javax.validation.Valid;
 import com.aurum.demographics.enums.ERole;
 import com.aurum.demographics.model.api.LoginRequest;
 import com.aurum.demographics.model.api.MessageResponse;
+import com.aurum.demographics.model.api.SignupRequest;
 import com.aurum.demographics.model.api.UserInfoResponse;
 import com.aurum.demographics.model.db.Role;
+import com.aurum.demographics.model.db.User;
 import com.aurum.demographics.repo.RoleRepository;
 import com.aurum.demographics.repo.UserRepository;
 import com.aurum.demographics.service.UserDetailsImpl;
@@ -69,56 +71,56 @@ public class AuthController {
                                    roles));
   }
 
-//  @PostMapping("/signup")
-//  public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-//    if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-//      return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
-//    }
-//
-//    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-//      return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
-//    }
-//
-//    // Create new user's account
-//    User user = new User(signUpRequest.getUsername(),
-//                         signUpRequest.getEmail(),
-//                         encoder.encode(signUpRequest.getPassword()));
-//
-//    Set<String> strRoles = signUpRequest.getRole();
-//    Set<Role> roles = new HashSet<>();
-//
-//    if (strRoles == null) {
-//      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//      roles.add(userRole);
-//    } else {
-//      strRoles.forEach(role -> {
-//        switch (role) {
-//        case "admin":
-//          Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-//              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//          roles.add(adminRole);
-//
-//          break;
-//        case "mod":
-//          Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-//              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//          roles.add(modRole);
-//
-//          break;
-//        default:
-//          Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//          roles.add(userRole);
-//        }
-//      });
-//    }
-//
-//    user.setRoles(roles);
-//    userRepository.save(user);
-//
-//    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-//  }
+  @PostMapping("/signup")
+  public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+    if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+    }
+
+    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+    }
+
+    // Create new user's account
+    User user = new User(signUpRequest.getUsername(),
+                         signUpRequest.getEmail(),
+                         encoder.encode(signUpRequest.getPassword()));
+
+    Set<String> strRoles = signUpRequest.getRole();
+    Set<Role> roles = new HashSet<>();
+
+    if (strRoles == null) {
+      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+      roles.add(userRole);
+    } else {
+      strRoles.forEach(role -> {
+        switch (role) {
+        case "admin":
+          Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+          roles.add(adminRole);
+
+          break;
+        case "mod":
+          Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+          roles.add(modRole);
+
+          break;
+        default:
+          Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+          roles.add(userRole);
+        }
+      });
+    }
+
+    user.setRoles(roles);
+    userRepository.save(user);
+
+    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+  }
 
   @PostMapping("/signout")
   public ResponseEntity<?> logoutUser() {
--- src/main/java/com/aurum/demographics/model/db/User.java
+++ src/main/java/com/aurum/demographics/model/db/User.java
@@ -25,7 +25,7 @@ public class User {
 
   @NotBlank
   @Size(max = 50)
-  @Email
+//  @Email
   private String email;
 
   @NotBlank
