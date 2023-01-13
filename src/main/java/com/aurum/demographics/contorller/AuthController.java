e51d66e90e06ce3493365feb0725ece2310bea60
santhoshkumr96 <santhoshkumr96@gmail.com>
2023-01-13 12:17:56 +0530

    add encoding validation

--- src/main/java/com/aurum/demographics/contorller/AuthController.java
+++ src/main/java/com/aurum/demographics/contorller/AuthController.java
@@ -84,7 +84,7 @@ public class AuthController {
     // Create new user's account
     User user = new User(signUpRequest.getUsername(),
                          signUpRequest.getEmail(),
-                         signUpRequest.getPassword());
+                         encoder.encode(signUpRequest.getPassword()));
 
     Set<String> strRoles = signUpRequest.getRole();
     Set<Role> roles = new HashSet<>();
