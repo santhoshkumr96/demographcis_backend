07d73946cba40f195fff8545768b58f35d2c2bad
santhoshkumr96 <santhoshkumr96@gmail.com>
2023-01-13 12:36:54 +0530

    remove passcode encoding and finish auth flow for login

--- src/main/java/com/aurum/demographics/security/WebSecurityConfig.java
+++ src/main/java/com/aurum/demographics/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -50,7 +51,8 @@ public class WebSecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+    return NoOpPasswordEncoder.getInstance();
+//    return new BCryptPasswordEncoder();
   }
   
   @Bean
