0b740fd645907891f20e05812f3ad66c3617d7ed
santhoshkumr96 <santhoshkumr96@gmail.com>
2023-04-03 12:17:53 +0530

    mem data changes

--- src/main/resources/dblogs/changelog/changes/24-alter-member-details-table-changelog.sql
+++ src/main/resources/dblogs/changelog/changes/24-alter-member-details-table-changelog.sql
@@ -16,8 +16,8 @@ ADD diabetic_enrollment_status varchar(100);
 alter table member_details
 ADD diabetic_enrollment_end_date timestamp;
 
-alter table member_details_audit
-ADD permanent_sterlization varchar(100);
+--alter table member_details_audit
+--ADD permanent_sterlization varchar(100);
 alter table member_details_audit
 ADD permanent_sterlization_date timestamp;
 alter table member_details_audit
